a086d79150c7f61f46e710a627e176275927f03b
Rez <rezhates@gmail.com>
2025-10-26 14:01:00 +0530

    First method but not the most efficient

---
+++ src/sections/Section1.jsx
@@ -0,0 +1,36 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import React, { useRef } from "react";
+
+const Section1 = ({ scrollProgress }) => {
+  const secRef = useRef(null);
+
+  useGSAP(() => {
+    const scale = gsap.utils.interpolate(1, 0.8, scrollProgress);
+    gsap.to(secRef.current, {
+      scale: scale,
+    });
+  },[scrollProgress]);
+  return (
+    <div
+      ref={secRef}
+      className=" sticky top-0 h-screen bg-[#451010] text-[3.5vw] flex flex-col items-center justify-center text-white pb-[10vh]"
+    >
+      <p>Scroll Perspective.</p>
+      <div className="flex gap-4">
+        <p>Section</p>
+        <div className="relative w-[12.5vw]">
+          <img
+            src="/images/pic1.jpg"
+            alt="img"
+            placeholder="blur"
+            className="w-full"
+          />
+        </div>
+        <p>Transition</p>
+      </div>
+    </div>
+  );
+};
+
+export default Section1;
