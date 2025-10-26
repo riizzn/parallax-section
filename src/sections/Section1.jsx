232f980453e034c04093654ac0819f64ce37e563
Rez <rezhates@gmail.com>
2025-10-26 14:52:22 +0530

    Add section 2 animation

--- src/sections/Section1.jsx
+++ src/sections/Section1.jsx
@@ -3,17 +3,19 @@ import gsap from "gsap";
 import React, { useRef } from "react";
 
 const Section1 = ({ scrollProgress }) => {
-  const secRef = useRef(null);
+  // const secRef = useRef(null);
 
-  useGSAP(() => {
+  // useGSAP(() => {
     const scale = gsap.utils.interpolate(1, 0.8, scrollProgress);
-    gsap.to(secRef.current, {
-      scale: scale,
-    });
-  },[scrollProgress]);
+    const rotate = gsap.utils.interpolate(0, -5, scrollProgress);
+  //   gsap.to(secRef.current, {
+  //     scale: scale,
+  //   });
+  // },[scrollProgress]);
   return (
     <div
-      ref={secRef}
+      // ref={secRef}
+      style={{transform: `scale(${scale}) rotate(${rotate}deg)`  }  }
       className=" sticky top-0 h-screen bg-[#451010] text-[3.5vw] flex flex-col items-center justify-center text-white pb-[10vh]"
     >
       <p>Scroll Perspective.</p>
--- src/sections/Section2.jsx
+++ src/sections/Section2.jsx
@@ -1,16 +1,22 @@
-import React from 'react'
+import gsap from "gsap";
+import React from "react";
 
-const Section2 = ({scrollProgress}) => {
+const Section2 = ({ scrollProgress }) => {
+  const scale = gsap.utils.interpolate(0.8, 1, scrollProgress);
+  const rotate = gsap.utils.interpolate(5, 0, scrollProgress);
   return (
-    <div  className="relative h-screen">
+    <div
+      style={{ transform: `scale(${scale}) rotate(${rotate}deg)` }}
+      className="relative h-screen"
+    >
       <img
-        src='/images/pic2.jpg'
+        src="/images/pic2.jpg"
         alt="img"
         placeholder="blur"
-        className='w-full h-full '
+        className="w-full h-full "
       />
     </div>
-  )
-}
+  );
+};
 
-export default Section2
+export default Section2;
